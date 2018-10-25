71853256349f5130e0485b42417f86641e5b7605
hamdyaea <hamdy.aea@gmail.com>
2018-10-25 21:14:37 +0200

    first commit

---
+++ start.py
@@ -0,0 +1,8 @@
+# Developer : Hamdy Abou El Anein
+
+from easygui import *
+
+image = "DayLightLogoSunSet.png"
+msg = "              Welcome to Daylight Linux\n\n\n              The lighter you become, the more you are able to run."
+choices = ["Daylight Linux"]
+buttonbox(msg, image=image, choices=choices)
