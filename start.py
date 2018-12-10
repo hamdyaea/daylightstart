fe969a68a44b66bab42976c5942cf69727601e89
hamdyaea <hamdy.aea@gmail.com>
2018-12-10 10:31:03 +0100

    add full path for pics

--- start.py
+++ start.py
@@ -3,7 +3,7 @@
 
 from easygui import *
 
-image = "DayLightLogoSunSet.gif"
+image = "/usr/share/daylight/daylightstart/DayLightLogoSunSet.gif"
 msg = "              Welcome to Daylight Linux\n\n\n              The lighter you become, the more you are able to run."
 choices = ["Daylight Linux"]
 buttonbox(msg, image=image, choices=choices)
